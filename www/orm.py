6fa687b60dd840394d73b5ed5ec20f5b71f5cb1d
JingxianFan <jingxian0317@gmail.com>
2017-05-02 00:04:07 -0400

    add models

--- www/orm.py
+++ www/orm.py
@@ -25,6 +25,13 @@ import aiomysql
     通过Field类将user类的属性映射到User表的列中，其中每一列的字段又有自己的一些属性，包括数据类型，列名，主键和默认值
  
 '''
+@asyncio.coroutine
+def destory_pool(): #销毁连接池
+    global __pool
+    if __pool is not None:
+        __pool.close()
+        yield from  __pool.wait_closed()
+        
  # 打印SQL查询语句
 def log(sql, args=()):
 	logging.info('SQL: %s' % sql)
@@ -38,8 +45,8 @@ def create_pool(loop, **kw):
 		port = kw.get('port', 3306),
 		user = kw['user'],
 		password = kw['password'],
-		db = kw['db'],
-		charset = kw.get('charset', 'utf-8'),
+		db = kw['database'],
+		charset = kw.get('charset', 'utf8'),
 		autocommit = kw.get('autocommit', True),
 		maxsize = kw.get('maxsize', 10),
 		minsize = kw.get('minsize', 1),
@@ -69,15 +76,17 @@ def execute(sql, args):
 	with (yield from __pool) as conn:
 		try:
 			# execute类型的三种SQL操作返回的只有行号，不用DictCursor
-			cur = yield from conn.cursor
+			cur = yield from conn.cursor()
 			yield from cur.execute(sql.replace('?', '%s'), args)
 			affected = cur.rowcount
 			yield from cur.close()
 		except BaseException as e:
-			# rollback() in case there was error
-			if not autocommit:
-				yield from conn.rollback()
+			# # rollback() in case there was error
+			# if not autocommit:
+			# 	yield from conn.rollback()
 			raise
+		finally:
+			conn.close()
 		return affected
 
 # 根据输入的参数生成占位符列表
@@ -214,7 +223,7 @@ class ModelMetaclass(type):
 
 class Model(dict, metaclass=ModelMetaclass):
 	def __init__(self, **kw):
-		super(model, self).__init__(**kw)
+		super(Model, self).__init__(**kw)
 
 	def __getattr__(self, key):
 		try:
@@ -260,59 +269,58 @@ class Model(dict, metaclass=ModelMetaclass):
 
 		limit = kw.get('limit', None)
 		if limit is not None:
-            sql.append('limit')
-            if isinstance(limit, int):
-                sql.append('?')
-                args.append(limit)
-            elif isinstance(limit, tuple) and len(limit) == 2:
-                sql.append('?, ?')
-                args.extend(limit)
-            else:
-                raise ValueError('Invalid limit value: %s' % str(limit))
-        rs = yield from select(' '.join(sql), args)
-        return [cls(**r) for r in rs]
-
-    @classmethod
-    @asyncio.coroutine
-    def findNumber(cls, selectField, where=None, args=None):
-        ' find number by select and where. '
-        sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
-        if where:
-            sql.append('where')
-            sql.append(where)
-        rs = yield from select(' '.join(sql), args, 1)
-        if len(rs) == 0:
-            return None
-        return rs[0]['_num_']
-    @classmethod
-    @asyncio.coroutine
-    def find(cls, pk):
-        ' find object by primary key. '
-        rs = yield from select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
-        if len(rs) == 0:
-            return None
-        return cls(**rs[0])
-
-    @asyncio.coroutine
-    def save(self):
-        args = list(map(self.getValueOrDefault, self.__fields__))
-        args.append(self.getValueOrDefault(self.__primary_key__))
-        rows = yield from execute(self.__insert__, args)
-        if rows != 1:
-            logging.warn('failed to insert record: affected rows: %s' % rows)
+			sql.append('limit')
+			if isinstance(limit, int):
+				sql.append('?')
+				args.append(limit)
+			elif isinstance(limit, tuple) and len(limit) == 2:
+				sql.append('?, ?')
+				args.extend(limit)
+			else:
+				raise ValueError('Invalid limit value: %s' % str(limit))
+		rs = yield from select(' '.join(sql), args)
+		return [cls(**r) for r in rs]
+	@classmethod
 	@asyncio.coroutine
-    def update(self):
-        args = list(map(self.getValue, self.__fields__))
-        args.append(self.getValue(self.__primary_key__))
-        rows = yield from execute(self.__update__, args)
-        if rows != 1:
-            logging.warn('failed to update by primary key: affected rows: %s' % rows)
+	def findNumber(cls, selectField, where=None, args=None):
+		' find number by select and where. '
+		sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		rs = yield from select(' '.join(sql), args, 1)
+		if len(rs) == 0:
+			return None
+		return rs[0]['_num_']
+	@classmethod
 	@asyncio.coroutine
-    def remove(self):
-        args = [self.getValue(self.__primary_key__)]
-        rows = yield from execute(self.__delete__, args)
-        if rows != 1:
-            logging.warn('failed to remove by primary key: affected rows: %s' % rows)
+	def find(cls, pk):
+		' find object by primary key. '
+		rs = yield from select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+		if len(rs) == 0:
+			return None
+		return cls(**rs[0])
+
+	@asyncio.coroutine
+	def save(self):
+		args = list(map(self.getValueOrDefault, self.__fields__))
+		args.append(self.getValueOrDefault(self.__primary_key__))
+		rows = yield from execute(self.__insert__, args)
+		if rows != 1:
+			logging.warn('failed to insert record: affected rows: %s' % rows)
+	@asyncio.coroutine
+	def update(self):
+		args = list(map(self.getValue, self.__fields__))
+		args.append(self.getValue(self.__primary_key__))
+		rows = yield from execute(self.__update__, args)
+		if rows != 1:
+			logging.warn('failed to update by primary key: affected rows: %s' % rows)
+	@asyncio.coroutine
+	def remove(self):
+		args = [self.getValue(self.__primary_key__)]
+		rows = yield from execute(self.__delete__, args)
+		if rows != 1:
+			logging.warn('failed to remove by primary key: affected rows: %s' % rows)
 
 
 
