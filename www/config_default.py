412d72e0b4f8b38357d0c93ae3fc6f03d002071c
JingxianFan <jingxian0317@gmail.com>
2017-05-09 01:20:31 -0400

    add configurations

---
+++ www/config_default.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3.5
+# -*- coding: utf-8 -*-
+
+'''
+Default configuration
+'''
+
+__author__ = 'JingxianFan'
+
+configs = {
+	'debug': True,
+	'db': {
+		'host': '127.0.0.1',
+		'port': 3306,
+		'user': 'www',
+		'password': 'www',
+		'db': 'awesome'
+	},
+	'session': {
+		'secret':'Awesome'
+	}
+}
---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.5
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'JingxianFan'
+
+configs = {
+	'db': {
+		'host': '127.0.0.1'
+	}
+}
